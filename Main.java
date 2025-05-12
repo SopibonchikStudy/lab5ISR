b61821db7a7d5d9265eb1c2a41870c811405c9b3
SopibonchikStudy <sopibonchikstudy@gmail.com>
2025-05-12 23:17:38 +0300

    Добавлено вычисление выражения

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public static void main(String[] args) {
     System.out.print("Это добавлено в ветке dev");
 
     for(int i = 0; i < 15; ++i) System.out.println("23");
+
+    System.out.println( 144 - 12 + 258);
 }
 }
