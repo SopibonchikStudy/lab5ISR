1bab79c192cb2131ca02f5c2cb005dc8eb35ba20
SopibonchikStudy <sopibonchikstudy@gmail.com>
2025-05-12 23:19:23 +0300

    Добавлено окончание

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
     System.out.println( 144 - 12 + 258);
 
     for(int i = 0; i < 10;++i) System.out.print('-');
+
+    System.out.println("The end");
 }
 }
