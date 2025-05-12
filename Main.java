fe73f66967b01bd0f5b1a007604c01edd83574b7
SopibonchikStudy <sopibonchikstudy@gmail.com>
2025-05-12 23:18:52 +0300

    Добавлено выведение 10 знаков '-' в строчку

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
     for(int i = 0; i < 15; ++i) System.out.println("23");
 
     System.out.println( 144 - 12 + 258);
+
+    for(int i = 0; i < 10;++i) System.out.print('-');
 }
 }
